db2ced07c53c5640e69bdfe785f755773e2f307f
Liperhf <stas.kuzora@mail.ru>
2025-07-25 16:49:56 +0300

    Some fixes

--- app/src/main/java/com/example/spotistats/presentation/screen/authorization/StatsScreen.kt
+++ app/src/main/java/com/example/spotistats/presentation/screen/authorization/StatsScreen.kt
@@ -1,19 +1,30 @@
 package com.example.spotistats.presentation.screen.authorization
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.example.spotistats.R
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun StatsScreen(navController: NavController){
+fun StatsScreen(navController: NavController,
+                viewModel:StatsViewModel){
     Scaffold() { paddingValues ->
         Text(text = stringResource(R.string.stats),
             modifier = Modifier.padding(paddingValues))
+    }
+    LaunchedEffect(Unit) {
+        viewModel.loadStats()
+    }
+    Scaffold(
+        
+    ) {paddingValues ->  
     }
 }
